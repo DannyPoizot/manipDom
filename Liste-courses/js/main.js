d6d5995317b5e3676f55fe151b34fb5c3e89c81d
DannyPoizot <poizot.dan@gmail.com>
2022-01-02 17:59:04 +0100

    delete and changing code, does not work, f*ck it

--- Liste-courses/js/main.js
+++ Liste-courses/js/main.js
@@ -1,48 +1,28 @@
 "use strict";
 
 // Html Elements
-let value = document.getElementsByTagName('input');
-let button = document.getElementsByTagName('button');
+let txt = document.querySelector('input');
+let button = document.querySelector('button');
 
 //Creating a list
-let ul = document.createElement('ul');
+let ul = document.querySelector('ul');
 let li = document.createElement('li');
+let p = document.createElement('p');
+let value = document.createTextNode('');
 
+txt.append(p);
+p.append(value);
+li.append(p);
+
+button.addEventListener('click', addValue);
 // New list element when the add button is clicked
 function addValue() {
-    if (value === '') {
+    if (value == '') {
       alert("Veuillez entrer une valeur !");
     } else {
       ul.appendChild(li);
     }
-    value.value = "";
 }
-
-// Close button on each list item
-for ( let i = 0; i < li.length; i++) {
-    let span = document.createElement('span');
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-// Click on "close" to hide a list element
-let span = document.createElement('span');
-span.className = 'close';
-for ( let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let article = this.parentElement;
-    article.style.display = "none";
-  }
-}
-
-// Click on element to "check it"
-ul.addEventListener('click', function(e){ 
-    if (e.target.tagName === 'li') {
-        e.target.classList.toggle('checked');
-    } false;
-});
 
 
 
