95a5304254f86ed1d219ddddadacf879940aaf65
DannyPoizot <poizot.dan@gmail.com>
2022-01-07 17:03:02 +0100

    functional monster slayer

--- README.md
+++ README.md
@@ -21,5 +21,11 @@ barres de vie :
     - démarrent toutes les deux à 100 pv
     - descendent en fonction des attaques
     - le nombre baisse aussi
-    - "progress bar"
-    - 
+    - barre de progression div 
+    - méthodes de modifications de la barre : div.style.width
+
+    div.scrollWidth
+
+    div.clientWidth
+
+    div.offsetWidth
---
+++ monster-slayer/js/main.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const sectionStart = document.querySelector(".sectionStart");
+const sectionGame = document.querySelector(".sectionGame");
+const progressPlayer = document.querySelector('.progressPlayer');
+const progressMonster = document.querySelector('.progressMonster');
+const pvPlayer = document.querySelector('.pvPlayer');
+const pvMonster = document.querySelector('.pvMonster');
+const btnStart = document.getElementById("sectionStart_btn");
+const btnAttack = document.getElementById("sectionGame_btn__attack");
+const btnSpecialAttack = document.getElementById("sectionGame_btn__special");
+const btnHeal = document.getElementById("sectionGame_btn__heal");
+const btnGiveUp = document.getElementById("sectionGame_btn__giveup");
+
+let pvP = 100;
+let pvM = 100;
+
+pvPlayer.textContent = pvP;
+pvMonster.textContent = pvM;
+
+sectionGame.hidden = true;
+
+btnStart.addEventListener('click', hideClickBtn);
+btnAttack.addEventListener('click', attack);
+btnSpecialAttack.addEventListener('click', specialAttack);
+btnHeal.addEventListener('click', heal);
+btnGiveUp.addEventListener('click', giveUp);
+
+
+function hideClickBtn(e) {
+    e.stopPropagation();
+    pvPlayer.textContent = pvP;
+    pvMonster.textContent = pvM;
+    sectionGame.hidden = false;
+    btnStart.hidden = true;
+    btnAttack.disabled = false;
+    btnSpecialAttack.disabled = false;
+    btnHeal.disabled = false;
+    progressMonster.style.width = pvM + '%';
+    progressPlayer.style.width = pvP + '%';
+}
+
+function damage(min, max) {
+    return Math.floor(Math.random() * (max - min+1) + min);
+}
+
+
+function attack() {
+    pvM = pvM - damage(3, 10);
+    pvP = pvP - damage(5, 10);
+    endGame();
+}
+
+
+function specialAttack() {
+    pvM = pvM - damage(10, 20);
+    pvP = pvP - damage(5, 10);
+    endGame();
+}
+
+function heal() {
+    pvP += 10;
+    afficher("heal");
+    pvP = pvP - damage(5, 10);
+    endGame();
+} 
+
+function giveUp() {
+    if(confirm("Want to give up ? Lol you loser")) {
+        pvP = 100;
+        pvM = 100;
+        sectionGame.hidden = true;
+        btnStart.hidden = false;
+        progressMonster.style.width = pvM + '%';
+        progressPlayer.style.width = pvP + '%';
+        pvPlayer.textContent = pvP;
+        pvMonster.textContent = pvM;
+    }
+}
+
+function endGame() {
+    if (pvM <= 0 ) {
+        pvM = 0;
+        win("Player");
+    } 
+    if (pvP <= 0) {
+        pvP = 0;
+        win("Monster");
+    } else if (pvP >= 100) {
+        pvP = 100;
+    }
+    pvPlayer.textContent = pvP;
+    pvMonster.textContent = pvM;
+    progressMonster.style.width = pvM + '%';
+    progressPlayer.style.width = pvP + '%';
+}
+
+function win(param) {
+    pvPlayer.textContent = pvP;
+    pvMonster.textContent = pvM;
+    progressMonster.style.width = pvM + '%';
+    progressPlayer.style.width = pvP + '%';
+    if(confirm(param  + ' won ! Do you want to start a new game ?')) {
+        pvP = 100;
+        pvM = 100;
+        sectionGame.hidden = true;
+        btnStart.hidden = false;
+        progressMonster.style.width = pvM + '%';
+        progressPlayer.style.width = pvP + '%';
+    } else {
+        btnAttack.disabled = true;
+        btnSpecialAttack.disabled = true;
+        btnHeal.disabled = true;
+    }
+} 
